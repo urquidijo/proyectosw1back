4bcd96f9ed1c9be1f06933e2d118fe7c53b7c011
urquidijo <josueurquidicarrillo@gmail.com>
2026-06-23 15:01:58 -0400

    cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:5500', 'http://localhost:5500', 'null'],
+    origin: ['http://localhost:3000', 'http://127.0.0.1:5500', 'http://localhost:5500', 'null','https://master.d1c68gzv00j6uq.amplifyapp.com'],
     credentials: true,
     // Sin esto el navegador no deja leer este header desde fetch() en el
     // frontend (no está en la lista de headers "seguros" por defecto de CORS),
